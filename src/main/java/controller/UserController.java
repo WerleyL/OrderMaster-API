a9d19dadc530079f164e395c7dac752470ee3498
WerleyL <werleyluciog@gmail.com>
2024-04-03 15:46:14 -0300

    Service layer, component registration

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,19 +1,31 @@
 package controller;
 
 import entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import services.UserService;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/users")
-public class UserController {
-    @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Maria", "MAria@gmail", "99999", "12345");
-        return ResponseEntity.ok().body(user);
 
+public class UserController {
+    @Autowired
+    private UserService service;
+    @GetMapping(value = "/users")
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+
+    }
+    @GetMapping(value = "/users{id}")
+    public ResponseEntity<User>findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 
--- src/main/java/repositories/UserRepository.java
+++ src/main/java/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package repositories;
 import entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
